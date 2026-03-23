48eb108606e037ac1e8acbfd77b138bf10840274
s10614165 <kweifon1998@gmail.com>
2026-03-23 16:28:23 +0800

    fix 修改元素錯誤擺放

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,4 @@
 import { RegistrationForm } from "./features/RegistrationForm";
-import headerBg from "./assets/image/header背景 1.svg";
-import footerBg from "./assets/image/footer背景 1.svg";
-
-const Header = () => (
-  <header className="w-full">
-    <img src={headerBg} alt="Header" className="w-full object-cover" />
-  </header>
-);
-
-const Footer = () => (
-  <footer className="w-full mt-auto">
-    <img src={footerBg} alt="Footer" className="w-full object-cover" />
-  </footer>
-);
 
 function App() {
   return (
@@ -21,7 +7,6 @@ function App() {
       <div className=" px-4 w-full flex justify-center">
         <RegistrationForm />
       </div>
-      {/* <Footer /> */}
     </div>
   );
 }
